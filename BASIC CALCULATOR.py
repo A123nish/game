eca3d8ae16f143f99fb79a6c9be31f84ecaae2ea
Anish raj parvat <51712849+A123nish@users.noreply.github.com>
2020-07-14 09:26:59 +0530

    Add files via upload
    calculator

---
+++ BASIC CALCULATOR.py
@@ -0,0 +1,6 @@
+#add two number[BASIC CALCULATOR]
+
+num1 =input ("Enter a number: ")
+num2 = input("Enter another number: ")
+result = float(num1) + float(num2)      #we have to give datatype for operation taken from user because it should understand that it isn't string
+print(result)
